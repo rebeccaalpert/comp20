e36fbf69346778fa8c3d7236dfa68e00b154c0ea
rebecca <rebecca.p.alpert@gmail.com>
2015-10-11 21:41:53 -0400

    Added init() function that displays background and sprite sheet on page load

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,13 @@
-// Replace this with your code...
+function init() {
+	var sprite = new Image();
+	var background = new Image();
+	sprite.addEventListener("load", function() {
+  		// execute drawImage statements here
+    	var canvas = document.getElementById("game_canvas");
+		var ctx = canvas.getContext("2d");
+		ctx.drawImage(background,0,0);
+		ctx.drawImage(sprite,0,0);
+		}, false);
+	background.src = "./duckhunt-background.gif";
+	sprite.src = "./duckhunt_various_sheet.png"; // Set source path
+}

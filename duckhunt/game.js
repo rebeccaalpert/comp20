8fae55833210e7541ef1b9396344dd9310404ffd
rebecca <rebecca.p.alpert@gmail.com>
2015-10-13 18:46:45 -0400

    Added two birds; positioned them on the background

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -6,7 +6,26 @@ function init() {
     	var canvas = document.getElementById("game_canvas");
 		var ctx = canvas.getContext("2d");
 		ctx.drawImage(background,0,0);
-		ctx.drawImage(sprite,0,0);
+		// draw first cropped bird
+        var sourceX = 0;
+        var sourceY = 118;
+        var sourceWidth = 38;
+        var sourceHeight = 34;
+        var destWidth = sourceWidth;
+        var destHeight = sourceHeight;
+        var destX = 80;
+        var destY = 70;
+        ctx.drawImage(sprite, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
+        // draw second cropped bird
+        var sourceX = 128;
+        var sourceY = 118;
+        var sourceWidth = 38;
+        var sourceHeight = 34;
+        var destWidth = sourceWidth;
+        var destHeight = sourceHeight;
+        var destX = 125;
+        var destY = 40;
+        ctx.drawImage(sprite, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
 		}, false);
 	background.src = "./duckhunt-background.gif";
 	sprite.src = "./duckhunt_various_sheet.png"; // Set source path
